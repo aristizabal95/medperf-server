1a0a8c21f92c3d9a162ce5e61732eed2d0eb95cc
Alejandro Aristizábal <alejandro.aristizabal@factored.ai>
2021-09-27 11:07:21 -0500

    Implement more cubes

--- app/main.py
+++ app/main.py
@@ -3,8 +3,17 @@ from fastapi.responses import FileResponse
 import yaml
 import os
 import aiofiles
+from pydantic import BaseModel
+from typing import Dict
 
 app = FastAPI()
+
+
+class Result(BaseModel):
+    benchmark_uid: str
+    model_uid: str
+    dataset_uid: str
+    scores: Dict[str, Dict[str, float]]
 
 
 @app.get("/benchmarks/{uid}")
@@ -43,6 +52,13 @@ async def get_cube_parameters(uid):
     return parameters_file
 
 
+@app.get("/cubes/{uid}/additional-files", response_class=FileResponse)
+async def get_cube_parameters(uid):
+    cube_path = get_cube_path(uid)
+    parameters_file = os.path.join(cube_path, "additional_files.tar.gz")
+    return parameters_file
+
+
 def get_cube_path(uid):
     cube_path = os.path.join("app/database/cubes", uid)
     if not os.path.isdir(cube_path):
@@ -56,4 +72,19 @@ async def upload_dataset(file: UploadFile = File(...)):
     async with aiofiles.open(write_path, "wb") as f:
         content = await file.read()
         await f.write(content)
+    return {"Result": "OK"}
+
+
+@app.post("/results")
+async def upload_result(result: Result):
+    bench = result.benchmark_uid
+    model = result.model_uid
+    dset = result.dataset_uid
+    scores = result.scores
+    write_path = os.path.join("app/database/results", bench, model, dset)
+    if not os.path.isdir(write_path):
+        os.makedirs(write_path)
+    write_path = os.path.join(write_path, "results.yaml")
+    with open(write_path, 'w') as f:
+        yaml.dump(scores, f)
     return {"Result": "OK"}
